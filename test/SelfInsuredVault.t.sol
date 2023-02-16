70e3974275c6890d40b10b90265f7ee011b1c27f
ortutay <marcell.ortutay@gmail.com>
2023-02-15 19:19:56 -0800

    Checkpoint

--- test/SelfInsuredVault.t.sol
+++ test/SelfInsuredVault.t.sol
@@ -2,17 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+
 import "y2k-earthquake/interfaces/IVault.sol";
 import "openzeppelin/token/ERC20/IERC20.sol";
+import "gmx/staking/interfaces/IRewardTracker.sol"
+
 import "../src/vaults/SelfInsuredVault.sol";
 
 contract SelfInsuredVaultTest is Test {
     uint256 arbitrumFork;
     string ARBITRUM_RPC_URL = vm.envString("ARBITRUM_RPC_URL");
 
-    address y2kUSDTVault = 0x76b1803530A3608bD5F1e4a8bdaf3007d7d2D7FA;
-    address usdt = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
-    address sGLP = 0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf;
+    IVault y2kUSDTVault = IVault(0x76b1803530A3608bD5F1e4a8bdaf3007d7d2D7FA);
+    IRewardTracker gmxRewardsTracker = IRewardTracker(0x4e971a87900b931ff39d1aad67697f49835400b6);
+
+    IERC20 usdt = IERC20(0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9);
+    IERC20 sGLP = IERC20(0x5402B5F40310bDED796c7D0F3FF6683f5C0cFfdf);
 
     address glpWallet = 0x3aaF2aCA2a0A6b6ec227Bbc2bF5cEE86c2dC599d;
 
@@ -36,20 +41,25 @@ contract SelfInsuredVaultTest is Test {
 
     function testCallToY2K() public {
         vm.selectFork(arbitrumFork);
-        address token = IVault(y2kUSDTVault).tokenInsured();
+        address token = y2kUSDTVault.tokenInsured();
         assertEq(token, usdt);
     }
 
     function testDepositWithdraw() public {
         vm.selectFork(arbitrumFork);
-        console.log(IERC20(sGLP).balanceOf(glpWallet));
+        console.log(sGLP.balanceOf(glpWallet));
 
         address user = createUser(0);
 
         vm.prank(glpWallet);
-        IERC20(sGLP).transfer(user, 10e18);
+        sGLP.transfer(user, 10e18);
 
-        console.log(IERC20(sGLP).balanceOf(glpWallet));
-        console.log(IERC20(sGLP).balanceOf(user));
+        console.log("claimable", gmxRewardsTracker.claimable(glpWallet));
+
+        console.log(sGLP.balanceOf(glpWallet));
+        console.log(sGLP.balanceOf(user));
+
+        console.log("claimable", gmxRewardsTracker.claimable(glpWallet));
+        console.log("claimable", gmxRewardsTracker.claimable(user));
     }
 }
